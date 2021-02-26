584ef282f00092c6592a3ce4bde9772797fd7f81
DP3PO <williampferebee@gmail.com>
2021-02-26 16:07:14 -0500

    Updates

---
+++ precache-manifest.b1f7fdfba6917fc99718979ea4d482e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82737f5ac09bed29bc6e9d7a826583be",
+    "url": "/HackerNewsAPI_Redux_React/index.html"
+  },
+  {
+    "revision": "84479589b526500777b2",
+    "url": "/HackerNewsAPI_Redux_React/static/css/main.fefc393e.chunk.css"
+  },
+  {
+    "revision": "e1e3d5e93a467b048414",
+    "url": "/HackerNewsAPI_Redux_React/static/js/2.91db8229.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HackerNewsAPI_Redux_React/static/js/2.91db8229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84479589b526500777b2",
+    "url": "/HackerNewsAPI_Redux_React/static/js/main.0f28b106.chunk.js"
+  },
+  {
+    "revision": "8551e9d79bd3c5355349",
+    "url": "/HackerNewsAPI_Redux_React/static/js/runtime-main.ccfaa6d3.js"
+  }
+]);
